e194d509277ea18d2031193155a7c0ee0a4e5461
Vasily93 <markovavasily@gmail.com>
2022-10-14 15:08:46 -0700

    refactored to use tag ids instead of names

--- src/TimeTracker.js
+++ src/TimeTracker.js
@@ -1,17 +1,16 @@
-import {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function TimeTracker() {
-  const [tags, setTags] = useState([{name: 'Uncotegorized', id: 0, totalTime: 0}, {name:'code', id: 1, totalTime: 0}, {name:'gym', id: 2, totalTime: 0}, {name:'bass', id: 3, totalTime: 0}]);
-  const [currentTag, setCurrentTag] = useState(tags[0].name);
-  const [timer, setTimer] = useState(0);
+  const [tags, setTags] = useState([{name: 'Uncotegorized', id: 12345, totalTime: 0}]);
+  const [currentTagID, setCurrentTagID] = useState(tags[0].id);
+  const [timer, setTimer] = useState(new Date().getTime());
+  const [newTag, setNewTag] = useState('')
 
-  useEffect(() => {
-    setTimer(new Date().getTime())
-  }, [])
-  
-  function updateTagInTags(currentTag, timeStamp) {
+
+  function updateTagInTags(tagID, timeStamp) {
     const updatedTags = tags.map(tag => {
-      if (tag.name === currentTag) {
+      if (tag.id === tagID) {
         tag.totalTime = tag.totalTime +=  (timeStamp - timer) 
       }
       return tag;
@@ -22,35 +21,44 @@ function TimeTracker() {
   function handleClick(clickedID) {
     const timeStamp =  new Date().getTime()
     setTimer(timeStamp)
-    setCurrentTag(prevTag => {
-      if(prevTag === clickedID) {
+    setCurrentTagID(prevTagID => {
+      if(prevTagID === clickedID) {
         updateTagInTags(clickedID, timeStamp)
-        return 'Uncotegorized'
-      } else if(prevTag !== clickedID) {
-        updateTagInTags(prevTag, timeStamp)
+        return 12345
+      } else if(prevTagID !== clickedID) {
+        updateTagInTags(prevTagID, timeStamp)
         return clickedID
       }
       
     })
   }
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    setTags([...tags, {name: newTag, id: uuidv4(), totalTime: 0}])
+    setNewTag('')
+  }
+
   return (
     <div className="App">
-      
-      <h3>Current Tag: {`${tags[0].name}: ${tags[0].totalTime/1000} seconds`}</h3>
-
-      <div>
+      <form onSubmit={e => handleSubmit(e)}>
+        <label htmlFor='new-tag'>New Tag: </label>
+        <input id='new-tag' value={newTag} onChange={e => setNewTag(e.target.value)} />
+        <button>Add {newTag}</button>
+      </form>
+      <ul>
       {tags.map(({name, id, totalTime}, index) => {
-        if(index === 0) return null;
-        return (<button 
+        if(index === 0) return <li key={id}><button>{name} : {Math.floor(totalTime/1000)} seconds</button></li>
+        return (<li key={id}><button 
           onClick={e => handleClick(e.target.id)}
-          id={name} key={id}
-          style={{backgroundColor: currentTag === name ? 'lightblue': 'white'}}
+          id={id}
+          style={{backgroundColor: currentTagID === id ? 'lightblue': 'white'}}
         >
           {name} : {Math.floor(totalTime/1000)} seconds
-        </button>)
+        </button></li>)
       })}
-    </div>
+    </ul>
+    <h3>Current Tag: {tags.find(tag => tag.id === currentTagID).name}</h3>
     </div>
   );
 }
